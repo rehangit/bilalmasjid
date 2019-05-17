893b3c78c668f62561c6e8c09bec6362970cd769
Rehan Ahmad <rehanahmad@gmail.com>
2019-05-18 00:16:13 +0100

    add clasp project for friday schedule doc

---
+++ friday-schedule-doc/Code.js
@@ -0,0 +1,19 @@
+function doGet() {
+  return HtmlService
+      .createTemplateFromFile('Index')
+      .evaluate()
+  .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
+}
+
+function getData() {
+  var id = DocumentApp.getActiveDocument().getId() ;
+  var url = "https://docs.google.com/document/d/"+id+"/export?format=html"
+  var param = { method: "get" };
+  var html = UrlFetchApp.fetch(url);
+  return html.getContentText();
+      
+}
+
+function test() {
+  Logger.log(getData());
+}
