9cc5365b4fb094fd104a24ec7bf19c46b54d569f
Rehan Ahmad <rehanahmad@gmail.com>
2019-05-18 03:29:30 +0100

    friday heading adjustment

--- friday-schedule-doc/Code.js
+++ friday-schedule-doc/Code.js
@@ -1,17 +1,15 @@
 function doGet() {
-  return HtmlService
-      .createTemplateFromFile('Index')
-      .evaluate()
-  .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
+  return HtmlService.createTemplateFromFile("index")
+    .evaluate()
+    .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
 }
 
 function getData() {
-  var id = DocumentApp.getActiveDocument().getId() ;
-  var url = "https://docs.google.com/document/d/"+id+"/export?format=html"
+  var id = DocumentApp.getActiveDocument().getId();
+  var url = "https://docs.google.com/document/d/" + id + "/export?format=html";
   var param = { method: "get" };
   var html = UrlFetchApp.fetch(url);
   return html.getContentText();
-      
 }
 
 function test() {
